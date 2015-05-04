ca884c5c058b642fe94bb7e425296c161c0d8e50
Manuel J. Tejada <manute32@gmail.com>
2015-05-03 21:19:14 -0400

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   
-  root 'welcome#index'
-  get '/about', to: 'static_pages#about'
+  root 'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
